6157dfe98f32ade12e3f9471515fda72af8b0eee
Bekhan22 <shoislom149@gmail.com>
2025-06-09 15:10:05 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Behruz</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, sint odio. Consequatur vitae a eveniet nobis. Tempore nam, vel corrupti nesciunt doloribus deleniti voluptatem veritatis maxime dolore reiciendis, laborum temporibus, cum sunt. Voluptatem fugit consequatur quasi exercitationem atque ipsum iure. Nesciunt dolor quas quos dolore vitae quidem odit blanditiis sit.
+      </p>
+      <ul>
+        <li>Html</li>
+        <li>Css</li>
+        <li>Javascript</li>
+        <li>Tailwind css</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default App
